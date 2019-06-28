80b1f6f0f66c5ad674159f99ff3a7b23578155da
Terence Then <chenjienan2009@gmail.com>
2019-06-28 01:33:36 -0700

    create new project

---
+++ cogito_log/android/app/src/main/java/com/tererencechen/cogito_log/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tererencechen.cogito_log;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
